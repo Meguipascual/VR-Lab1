ab6eb863585473e089ffa2e02b269cafd29dee07
Meguipascual <meguipascual@gmail.com>
2026-01-29 14:26:49 +0100

    moving targets working hurra!!

---
+++ VR Lab1/Assets/Scripts/Vr/MovingTarget.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingTarget : MonoBehaviour
+{
+    [SerializeField] private GameObject _targetStart;
+    [SerializeField] private GameObject _targetEnd;
+    [SerializeField] private float _speed;
+    private bool _isFacingEnd;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (_targetStart == null || _targetEnd == null) { return; }
+        transform.position = _targetStart.transform.position;
+        _isFacingEnd = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_targetStart == null || _targetEnd == null) { return; }
+
+        if (_isFacingEnd)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _targetEnd.transform.position, _speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _targetStart.transform.position, _speed * Time.deltaTime);
+        }
+
+        if (transform.position == _targetStart.transform.position || transform.position == _targetEnd.transform.position)
+        {
+            _isFacingEnd = !_isFacingEnd;
+        }
+    }
+}
