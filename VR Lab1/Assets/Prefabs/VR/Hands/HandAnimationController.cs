d490540bf45443589530ad3d6160f28cfdae9b2f
Meguipascual <meguipascual@gmail.com>
2026-02-18 21:06:25 +0100

    SteamGirl with animations

---
+++ VR Lab1/Assets/Prefabs/VR/Hands/HandAnimationController.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandAnimationController : MonoBehaviour
+{
+    private Animator _animator;
+    private bool _isNull = true;
+    private bool _isFirstCall = true;
+    // Start is called before the first frame update
+    private void TrySearch()
+    {
+        _isFirstCall = false;
+        _animator = GetComponentInChildren<Animator>();
+        if (_animator != null)
+        {
+            _isNull = false;
+        }
+        else
+        {
+            Debug.Log($"Animator is Null");
+        } 
+    }
+
+    public void PlayPulseThumb() 
+    {
+        
+        if (_isNull) 
+        {
+            TrySearch();
+            return;
+        }
+        _animator.Play("PulseThumb");
+    }
+
+    public void PlayReleaseThumb()
+    {
+        if (_isNull)
+        {
+            TrySearch();
+            return;
+        }
+        _animator.Play("ReleaseThumb");
+    }
+    public void PlayPulseIndex()
+    {
+
+        if (_isNull)
+        {
+            TrySearch();
+            return;
+        }
+        _animator.Play("PulseIndex");
+    }
+    public void PlayReleaseIndex()
+    {
+        if (_isNull)
+        {
+            TrySearch();
+            return;
+        }
+        _animator.Play("ReleaseIndex");
+    }
+}
