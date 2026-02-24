abba3dbd12dc70aee94dd1be19fb409df4da2b7f
Meguipascual <meguipascual@gmail.com>
2026-02-24 16:53:39 +0100

    Hand controller actualization

--- VR Lab1/Assets/Prefabs/VR/Hands/HandAnimationController.cs
+++ VR Lab1/Assets/Prefabs/VR/Hands/HandAnimationController.cs
@@ -25,65 +25,79 @@ public class HandAnimationController : MonoBehaviour
     public void PlayPulseThumb() 
     {
         
-        if (_isNull) 
+        if (_isNull && _isFirstCall) 
         {
             TrySearch();
-            return;
         }
-        _animator.Play("PulseThumb");
+
+        if (_animator.isActiveAndEnabled)
+        {
+            _animator.Play("PulseThumb");
+        }
     }
 
     public void PlayReleaseThumb()
     {
-        if (_isNull)
+        if (_isNull && _isFirstCall)
         {
             TrySearch();
-            return;
         }
-        _animator.Play("ReleaseThumb");
+
+        if (_animator.isActiveAndEnabled)
+        {
+            _animator.Play("ReleaseThumb");
+        }
     }
+
     public void PlayPulseIndex()
     {
-
-        if (_isNull)
+        if (_isNull && _isFirstCall)
         {
             TrySearch();
-            return;
         }
-        _animator.Play("PulseIndex");
+
+        if (_animator.isActiveAndEnabled)
+        {
+            _animator.Play("PulseIndex");
+        }
     }
+
     public void PlayReleaseIndex()
     {
-        if (_isNull)
+        if (_isNull && _isFirstCall)
         {
             TrySearch();
-            return;
         }
-        _animator.Play("ReleaseIndex");
+
+        if (_animator.isActiveAndEnabled)
+        {
+            _animator.Play("ReleaseIndex");
+        }
     }
 
     public void PlayPulseOthers()
     {
-
-        if (_isNull)
+        if (_isNull && _isFirstCall)
         {
             TrySearch();
-            return;
         }
-        _animator.Play("PulseMid");
-        _animator.Play("PulseRing");
-        _animator.Play("PulsePinky");
+
+        if (_animator.isActiveAndEnabled)
+        {
+            _animator.Play("PulseLower");
+        }
     }
 
     public void PlayReleaseOthers()
     {
-        if (_isNull)
+        if (_isNull && _isFirstCall)
         {
             TrySearch();
-            return;
         }
-        _animator.Play("ReleaseMid");
-        _animator.Play("ReleaseRing");
-        _animator.Play("ReleasePinky");
+
+        if (_animator.isActiveAndEnabled)
+        {
+            _animator.Play("ReleaseLower");
+        }
     }
 }
